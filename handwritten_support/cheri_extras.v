46f3ea9917427f7aaa64a8d601c5ef8a46adf586
Brian Campbell <Brian.Campbell@ed.ac.uk>
2019-08-19 11:28:35 +0100

    Coq cheri_extras can now use bool_of_bitU_nondet

--- handwritten_support/cheri_extras.v
+++ handwritten_support/cheri_extras.v
@@ -5,11 +5,9 @@ Require Import Sail2_prompt_monad.
 Require Import Sail2_prompt.
 
 Definition write_tag_bool {rv A E} (addr : mword A) tag : monad rv unit E :=
-  bind (bind (read_memt Read_plain addr 16)
-             (fun '(cap, _) =>
-                write_memt Write_plain addr 16 cap (bitU_of_bool tag)))
-       (fun _ => Done tt).
+ read_memt Read_plain addr 16 >>= fun '(cap, _) =>
+ write_memt Write_plain addr 16 cap (bitU_of_bool tag) >>= (fun _ => returnm tt).
 
 Definition read_tag_bool {rv E} (addr : mword 64) : monad rv bool E :=
-  bind (read_memt Read_plain addr 16)
-       (fun '(cap, tag) => returnm (bool_of_bitU tag)). (* FIXME: need bool_of_bitU_nondet here? *)
+  read_memt (B := 128) Read_plain addr 16 >>= fun '(cap, tag) =>
+  bool_of_bitU_nondet tag.
