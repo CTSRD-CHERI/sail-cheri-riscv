e2ac602e743565232ac509fea7ab45602da4506c
Robert Norton <robert.norton@microsoft.com>
2021-04-30 11:02:53 +0100

    Tweaks to make test work with QEMU.

--- test/baremetal/epilogue.s
+++ test/baremetal/epilogue.s
@@ -5,7 +5,9 @@
 success:
 # Write 1 to tohost, meaning exit with code 0
     li t0, 1
-    sw t0, tohost, t1
+    la t1, tohost
+    sw t0, 0(t1)
+    sw x0, 4(t1)
 # Spin until termination.
 1:  j 1b
 
@@ -13,16 +15,21 @@ success:
 fail:
 # Write 3 to tohost, meaning exit with code 1  (lsb=1 means exit, upper bits=exit code).
     li t0, 3
-    sw t0, tohost, t1
+    la t1, tohost
+    sw t0, 0(t1)
+    sw x0, 4(t1)
 # Spin until terminatation
 1:  j 1b
 
 .section ".tohost", "aw", @progbits
 .align 6
 .globl tohost
+.size tohost, 8
 tohost:
     .dword 0
+	
 .align 6
 .globl fromhost
+.size fromhost, 8
 fromhost: 
     .dword 0
